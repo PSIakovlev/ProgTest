aca5ae30152eb7153f1da4b24ddf5e254558b7b4
Pavel <ikpas21041997@mail.ru>
2025-04-08 18:32:49 +0500

    Fixed number conversion and handled cases with empty file and missing column

--- main.py
+++ main.py
@@ -3,17 +3,22 @@ import pandas as pd
 
 def phone_to_hash(phone: str) -> str:
     """Функция преобразования номера в хэш"""
-    phone = phone.replace("8985", "+7985-").replace("+7985", "+7985-")
+    phone = phone.replace("+7985", "+7985-").replace("8985", "+7985-")
     return phone
 
 
-def process_csv(input_file: str, output_file: str) -> None:
+def process_csv(input_file: str) -> None:
     """Функция чтения файла и его обработки"""
     with open(input_csv, "r") as f:
         format_encode = f.encoding
-
+        if f.read().strip() == "":
+            print("Пустой файл")
+            return
         data = pd.read_csv(input_file, encoding=format_encode, sep=";")
         result_list = []
+        if "Номер телефона" not in data:
+            print("Нет нужного заголовка")
+            return
 
         for number in data["Номер телефона"]:
             hash = phone_to_hash(str(number))
@@ -23,7 +28,6 @@ def process_csv(input_file: str, output_file: str) -> None:
 
 
 input_csv = 'input.csv'
-output_csv = 'output.csv'
 
-process_csv(input_csv, output_csv)
+process_csv(input_csv)
 
